09451f4a2e7635520413ebd1195f023e5a7ec37d
chengwei ou <chengweiou@chengweis-MacBook-Pro.local>
2021-07-03 00:43:56 -0700

    merge

--- src/router.js
+++ src/router.js
@@ -3,3 +3,8 @@ import { createRouter, createWebHistory } from 'vue-router'
 export const router = createRouter({
   history: createWebHistory(),
   routes: [
+    { name: 'home', path: '/', component: () => import('@/view/home/index.vue') },
+    { name: 'detail', path: '/detail', component: () => import('@/view/detail/index.vue') },
+    { name: 'log', path: '/log', component: () => import('@/view/log') },
+  ],
+})
